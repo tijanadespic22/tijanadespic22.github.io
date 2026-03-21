c1969730fc83bfb12fe2653aa0539a774b512ed6
Vladimir Antin <antin502@gmail.com>
2026-03-21 10:12:48 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // <=== enables static exports
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
-    turbopack: {
-        resolveAlias: {
-            canvas: false,
-        },
-    },
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
