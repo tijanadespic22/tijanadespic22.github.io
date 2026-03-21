2f6a3494ddd6b3a97a32cf7f1f99e822f7f33eb7
Vladimir Antin <antin502@gmail.com>
2026-03-21 10:07:45 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // <=== enables static exports
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config: any) => {
+        config.resolve.alias.canvas = false;
+        return config;
+    },
 };
 
 export default nextConfig;
