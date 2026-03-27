09f20e794013896b86c5d0498290c957acdb4c1f
Vladimir Antin <antin502@gmail.com>
2026-03-27 07:43:09 +0100

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type {MetadataRoute} from 'next';
 
+export const dynamic = 'force-static';
+
 const BASE_URL = 'https://tijanadespic.hok.rs';
 
 export default function sitemap(): MetadataRoute.Sitemap {
